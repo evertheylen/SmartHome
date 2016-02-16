b3ba56bc5f8392be8a8f9dbef15343b0cf5972b5
stijnjanssens <janssens.stijn@hotmail.com>
2016-02-16 19:48:56 +0100

    voort aant zooien

--- playground/stijn/overwatch_angular.js
+++ playground/stijn/overwatch_angular.js
@@ -1,9 +1,9 @@
-var app = angular.module("overwatch", []);
+var app = angular.module("overwatch", ["ngRoute"]);
 
 app.controller("mainCtrl", function($scope) {
   $scope.user = {first_name: "Stijn", last_name: "Janssens"};
   $scope.html_strings = {
-    "home": ["Home ", "Start "], "contact": ["Contact ", "Contact "], "about": ["About ", "Over "], "language": ["Language", "Taal"],
+    "home": ["Home ", "Start "], "contact": ["Contact ", "Contacteer "], "about": ["About ", "Over "], "language": ["Language", "Taal"],
     "register": ["Register", "Registreer"], "login": ["Login", "Inloggen"], "homepage_welcome": ["Welcome to", "Welkom bij "],
     "username": ["Username", "Gebruikersnaam"], "password": ["Password", "Paswoord"]};
   $scope.language = 0;
@@ -12,3 +12,19 @@ app.controller("mainCtrl", function($scope) {
     return $scope.html_strings[input][$scope.language];
   };
 });
+
+app.config(["$routeProvider", "$locationProvider",
+  function($routeProvider, $locationProvider){
+    $routeProvider.when("/", {
+      templateUrl: "partials/home.html"
+    }).when("/contact", {
+      templateUrl: "partials/contact.html"
+    }).when("/about", {
+      templateUrl: "partials/about.html"
+    }).when("/register", {
+      templateUrl: "partials/register.html"
+    }).otherwise({
+      redirectTo: "/"
+    });
+    //$locationProvider.html5Mode(true);
+}]);
