186673441be532edde8508e72f7e005260e248ba
babbel056 <hermans.anthony@hotmail.com>
2016-02-22 20:09:44 +0100

    added a todo for backend guys

--- code/run.py
+++ code/run.py
@@ -20,7 +20,7 @@ define("debug", default=True, help="run in debug mode")
 
 def localdir(location):
     return os.path.join(os.path.dirname(__file__), location)
-
+# TODO close the ioLoop properly so no errors occur when KeyboardInterrupt occurs
 class OverWatch():
     def __init__(self, tornado_app_settings={}):
         # Logging support
@@ -29,16 +29,16 @@ class OverWatch():
         # HOW TO LOG:
         self.logger.debug("OverWatch initialized")
         # Or use one of these functions: info, warn, error, critical
-        
+
         # The view/presentation layer are the Handlers, so it's kinda the 'app' provided by Tornado.
         # Every handler will get a reference to the controller
         self.controller = Controller(self.logger, None, None)
-        
+
         # The model is pretty self-explanatory
         # The database is not managed by the model, but it's not a big deal really.
         # It could be managed by the model, but I prefer to keep the model clean of that.
         self.model = Model(self.logger, self.controller)
-        
+
         # Now of course, set the controllers references too.
         self.controller.model = self.model
         self.controller.db = Database(self.logger)
@@ -46,7 +46,7 @@ class OverWatch():
         # In a classical app, it does not need too. In our app however, we need two-way communication.
         # However, WebsocketHandlers will signal a new connection to the controller, and the controller
         # will maintain that list.
-        
+
         # This is the Tornado Application. "But wait, it's an object! No subclassing? And what about
         # those weird functions create_...Handler?"
         # You may expect an important class like an 'Application' to subclassed into our own application.
@@ -66,7 +66,7 @@ class OverWatch():
             ],
             **tornado_app_settings
         )
-        
+
     def run(self):
         self.logger.info("Starting OverWatch on: http://localhost:%d/"%options.port)
         self.app.listen(options.port)
@@ -82,7 +82,3 @@ if __name__ == "__main__":
     except KeyboardInterrupt:
         tornado.ioloop.IOLoop.current().stop()
         ow.logger.info("Stopping because of KeyboardInterrupt")
-    
-        
-        
-
