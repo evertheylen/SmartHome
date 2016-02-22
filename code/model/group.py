82e407f64c34550035f350c94d661548397131e0
babbel056 <hermans.anthony@hotmail.com>
2016-02-22 20:00:03 +0100

    added basic classes in folder model and added  minor adaption to database.py

---
+++ code/model/group.py
@@ -0,0 +1,10 @@
+class Group:
+    def __init__(self, ID, name, wall_ID):
+        self.ID = ID
+        self.name = name
+        self.wall_ID = wall_ID
+
+    def toDict(self):
+        # to dict representation, not sure what the specific representation is yet :/
+        # TODO pass for now
+        pass
