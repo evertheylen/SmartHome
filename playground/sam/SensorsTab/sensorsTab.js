5cc5ae3fa7f74af9e676676c57c3b5ff23f9c3fb
SamMylle <mylle.sam@gmail.com>
2016-02-24 14:43:46 +0100

    Dialog add Location

--- playground/sam/SensorsTab/sensorsTab.js
+++ playground/sam/SensorsTab/sensorsTab.js
@@ -1,9 +1,21 @@
 /*
 	Stijn i'm sure you'll do this better than me, i did this just for testing
+	TODO connect the correct buttons to the correct dialog (same as in the login screen)
 */
 
 var dialog = document.getElementById('dlgSensor');
 var showDialogButton = document.getElementById('btnAddSensor');
+if (! dialog.showModal) {
+dialogPolyfill.registerDialog(dialog);
+}
+showDialogButton.addEventListener('click', function() {
+dialog.showModal();
+});
+
+
+
+var dialog = document.getElementById('dlgLocation');
+var showDialogButton = document.getElementById('btnAddLoc');
 if (! dialog.showModal) {
 dialogPolyfill.registerDialog(dialog);
 }
