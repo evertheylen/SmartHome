79a1e1c718c0e7f1474a80bb61442fab5a184acd
babbel056 <hermans.anthony@hotmail.com>
2016-02-25 19:28:35 +0100

    logout

--- code/controller/controller.py
+++ code/controller/controller.py
@@ -22,23 +22,26 @@ class Controller:
 
         self.sessions = {}
         # Session --> UID
-        
+
         self.listeners = defaultdict(lambda: defaultdict(set))  # TODO
         # Type --> ID --> {jef, jos, maria}
-    
+
     def get_user(self, session):
         if session in self.sessions:
             return self.sessions[session]
         else:
             return None
-    
+
     # Logout: remove session from self.sessions
-    
+    async def logout(self, req):
+            self.sessions.pop(req.conn.session)
+
+
     async def login(self, req):
         res = await self.db.get(User.table_name, "email", req.dct["data"]["email"])
         if res is None: return
         u = User.from_db(res)
-        
+
         # TODO IMPORTANT Don't store plaintext passwords
         if u.password == req.dct["data"]["password"]:
             session = hashlib.md5(bytes(str(random.random())[2:] + "WoordPopNoordzee", "utf8")).hexdigest()
@@ -48,7 +51,7 @@ class Controller:
             await req.answer({"session": session})
         else:
             await req.answer("fail")
-    
+
     async def signup(self, req):
         res = await self.db.get(User.table_name, "email", req.dct["data"]["email"])
         if res is not None:
@@ -58,18 +61,16 @@ class Controller:
             self.logger.debug("data = " + repr(req.dct["data"]))
             u = await User.new(self.db, req.dct["data"])
             await req.answer("success")
-    
+
     async def handle_request(self, req):
         if req.dct["type"] in Controller.__dict__:
             await Controller.__dict__[req.dct["type"]](self, req)
         else:
             self.logger.error("No handler for %s in Controller"%req.dct["type"])
-    
+
     async def _handle_message(self, conn, dct):
         u = await User.new(self.db, {"first_name": "Evert", "last_name": "Heylen", "password": "123", "email": "e@e"})
         await conn.send({"answer": u.to_dict()})
         await u.set(self.db, "first_name", "Anthony")
         new_u = User.get(self.db, u.UID)
         await conn.send({"user": u.to_dict()})
-        
-        
