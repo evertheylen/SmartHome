e83278c21ed43b6b92b9b0a4f1c01fba63873384
Evert Heylen <evertheylen@gmail.com>
2016-03-10 20:36:41 +0100

    no more caching index.html

--- code/handlers/mainhandler.py
+++ code/handlers/mainhandler.py
@@ -2,11 +2,11 @@ import tornado
 
 # Handlers are instantiated for every request!
 def create_MainHandler(controller):
-    f = open("html/index.html", "r")
-    content = str(f.read())
-    
+    # TODO cache html/index.html
     class MainHandler(tornado.web.RequestHandler):
         def get(self, *args):
+            f = open("html/index.html", "r")
+            content = str(f.read())
             self.write(content)
             
         def post(self, *args):
