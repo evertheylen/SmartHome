82d8c7ecab1852c91bc6d63010d1ec9aa008260b
Stijn Janssens <janssens.stijn@hotmail.com>
2016-03-17 11:18:45 +0100

    sort pagination

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -1137,7 +1137,7 @@ angular.module("overwatch").controller("sensorController", function($scope, $roo
 			addClass(document.getElementById(elementId), "down");
 		}
 		$filter('orderBy')($scope.sensors, orderBy, $scope.selected_order === orderBy);
-	    console.log($scope.sensors.name);
+	    console.log($scope.sensors['name']);
 	}
     
 	$scope.reset_loc = function reset_loc() {
