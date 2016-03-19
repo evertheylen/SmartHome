703a9081acac6fb479d3b10876b29417d85338b6
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-03-19 17:34:19 +0100

    bugfix

--- code/js/prototypes/user.js
+++ code/js/prototypes/user.js
@@ -3,7 +3,7 @@ User.prototype.constructor = User;
 
 function User(UID, first_name, last_name, email) {
 	this.UID = UID;
-	this.first_name = firstName;
-	this.last_name = lastName;
+	this.first_name = first_name;
+	this.last_name = last_name;
 	this.email = email;
 }
