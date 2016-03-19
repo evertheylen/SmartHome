41c56750a5bb3812926726637ef3c6d93ae00ee6
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-03-19 17:13:07 +0100

    bugfix

--- code/js/prototypes/data_type.js
+++ code/js/prototypes/data_type.js
@@ -3,8 +3,10 @@ function DataType() {
 		var tmp = {};
 
 		for(var key in this) {
-			if(typeof this[key] !== 'function')
+			if(typeof this[key] !== 'function') {
+				console.log(key);
 				tmp[key] = this[key];
+			}
 		}
 
 		return tmp;
