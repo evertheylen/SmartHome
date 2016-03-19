43b0efec89d3967a0224a82b308de74e3358bc56
Stijn Janssens <janssens.stijn@hotmail.com>
2016-03-19 16:01:41 +0100

    Login refresh

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -24,7 +24,7 @@ angular.module("overwatch").controller("sensorController", function($scope, $roo
 	*/
 	$scope.sensors = [];
 	console.log("Empty array: " + $scope.sensors);
-    console.log("Pre request auth_user == " + $rootScope.auth_user);
+    console.log("Pre request auth_user == " + $rootScope.auth_user.id);
 	ws.request({type: "get_all", what: "Sensor", for: {what: "User", UID: $rootScope.auth_user.id}}, function(response) {
 	    
 		$scope.sensors = response.sensors;
