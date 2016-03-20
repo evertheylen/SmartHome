9d4f969eb219d12f5055b8121dffa044fe067eb0
Evert Heylen <evertheylen@gmail.com>
2016-03-20 22:24:38 +0100

    Update wshandler.py

--- code/handlers/wshandler.py
+++ code/handlers/wshandler.py
@@ -45,7 +45,10 @@ def create_WsHandler(controller):
             controller.logger.info("Server opened connection")
             clients.add(self)
             self.session = self.get_cookie("session")
-            self.user = controller.get_user(self.session)
+            tornado.ioloop.IOLoop.current().spawn_callback(controller.open_async, self)
+        
+        async def open_async(self):
+            self.user = await controller.get_user(self.session)
         
         # Reminder: the docs say this function *must* be synchronous
         def on_message(self, message):
