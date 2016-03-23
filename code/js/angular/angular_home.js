a869857aea3f09a141c05450d8a054de56982375
Stijn Janssens <janssens.stijn@hotmail.com>
2016-03-23 11:29:29 +0100

    first chart

--- code/js/angular/angular_home.js
+++ code/js/angular/angular_home.js
@@ -21,115 +21,23 @@ angular.module("overwatch").controller("homeController", function($scope, $rootS
 	};
     $scope.graphs = []
     
-    barchart = {};
-    barchart.type = "BarChart";
+    graph = {};
+    graph.type = "chart-line";
+    graph.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    graph.series = ['Series A', 'Series B'];
+    graph.data = [
+        [65, 59,80,81,56,55,40,59,54,53,30,12],
+        [28,48,40,19,86,27,90,456,78,45,01,45]
+    ];
     
-    barchart.data = {"cols": [{id: "t", label: "Topic", type: "string"},
-    {id: "a", label: "Amount", type: "number"}],
-     "rows": [{c: [{v: "University"},{v: 20},]},{c: [{v: "Programming"},{v: 50},]},{c: [{v: "Databases"},{v: 100},]},{c: [{v: "Work"},{v: 40},]},{c: [{v: "This Epic Chart"},{v: 560},]}]};
-    
-    barchart.options = {
-        'title': "Things I Like",
-        "hAxis": {
-            "gridlines": {
-                "count": 10
-            }
-        }
-    };
-    
-    $scope.graphs.push(barchart);
-    
-    for (var i = 0; i < 3; i ++ ) {
-        graph = {};
-        graph.type = "LineChart";
-        graph.displayed = false;
-        graph.data = {
-            "cols": [{
-                id: "year",
-                label: "Year",
-                type: "number"
-            }, {
-                id: "importance",
-                label: "Importance",
-                type: "number"
-            }, {
-                id: "sales",
-                label: "Sales",
-                type: "number"
-            }], 
-            "rows" : [{
-                c: [{
-                    v: 2011
-                }, {
-                    v: 0
-                }, {
-                    v: 3100
-                }]
-            }, {
-                c: [{
-                    v: 2012
-                }, {
-                    v: 200
-                }, {
-                    v: 3000
-                }]
-            }, {
-                c: [{
-                    v: 2013
-                }, {
-                    v: 400
-                }, {
-                    v: 2800
-                }]
-            }, {
-                c: [{
-                    v: 2014
-                }, {
-                    v: 800
-                }, {
-                    v: 2400
-                }]
-            }, {    
-                c: [{
-                    v: 2015
-                }, {
-                    v: 1600
-                }, {
-                    v: 1600
-                }]
-            }, {     
-                c: [{
-                    v: 2016
-                }, {
-                    v: 3200
-                }, {
-                    v: 0
-                }]
-            }]
-        };
-        graph.options = {
-            "title": "Sales Of Important Stuff",
-            "colors": ['#FF0000', '#0000FF'],
-            "defaultColors": ['#FF0000', '#0000FF'],
-            "isStacked": "true",
-            "fill": 20,
-            "displayExactValues": true,
-            "vAxis": {
-                "title": "Values",
-                "gridlines": {
-                    "count": 4
-                }
-            },
-            "hAxis": {
-                "title": "Year"
-            }
-        };
-        graph.view = {
-            colums: [0,1,2]
-        };
+    $timeout(function () {
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90, 59,54,53,30,12],
+      [65, 59, 80, 81, 56, 55, 40, 456,78,45,01,45]
+    ];
+  }, 5000);
         
-        $scope.graphs.push(graph);  
-    };
+    $scope.graphs.push(graph);  
 
 	componentHandler.upgradeDom();
 });	
