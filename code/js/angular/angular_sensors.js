877252ba553a04911c4432e6179c68efcff07d75
stijnjanssens <janssens.stijn@hotmail.com>
2016-03-24 13:47:53 +0100

    Automatic scaling of pagination buttons

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -1066,7 +1066,7 @@ angular.module("overwatch").controller("sensorController", function($scope, $roo
 		var begin = (($scope.currentPage - 1) * $scope.numPerPage)
 		, end = begin + $scope.numPerPage;
 		
-		if ($scope.sensors.length-1 < $scope.numPerPage * $scope.maxSize) {
+		if ($scope.sensors.length-1 < $scope.numPerPage * ($scope.maxSize-1)) {
 			var length = Math.floor(($scope.sensors.length-1)/$scope.numPerPage)+1;
 			console.log("Calculating paging length: fl(" + ($scope.sensors.length-1) + "/" + $scope.numPerPage + ")+1 = "+ length);
 			$scope.pages_css = "pagination--length" + length;
