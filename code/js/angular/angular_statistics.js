6ea03335844f44ddebeaa85a3ef1b61aa62eb638
Stijn Janssens <janssens.stijn@hotmail.com>
2016-03-28 14:12:17 +0200

    Statistics tab

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -4,7 +4,8 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
     $rootScope.page_title = "OverWatch - " + $scope.i18n($rootScope.tab);
     
     // Sample data
-    $scope.all_locs= true;
+    var all_locs= false;
+    
     $scope.locations = [{"desc": "Campus Middelheim", "country": "Belgium", "city": "Antwerp", "postalcode": 2020, "street": "Middelheimlaan", "number": 1}, 
 			    {"desc": "Campus Groenenborger", "country": "Belgium", "city": "Antwerp", "postalcode": 2020, "street": "Groenenborgerlaan", "number": 171}, 
 			    {"desc": "Campus Drie Eiken", "country": "Belgium", "city": "Antwerp", "postalcode": 2610, "street": "Universiteitsplein", "number": 1}];
@@ -16,6 +17,14 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
     for (i = 0; i < $scope.locations.length; i++) {
         $scope.select_loc.push(false);
     }
+
+    $scope.all_locations = function () {
+        all_locs != all_locs;
+        for (i=0; i < $scope.select_loc.length; i++) {
+            $scope.select_loc[i] = all_locs;
+        }
+    }; 
+ 
     //Aggregation:
     /*
     [bool : aggregate_location, bool: aggregate_type, bool: aggregate_sensor]
