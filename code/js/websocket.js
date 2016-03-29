57431c66717319f5411781eaddb6c5b4a0ba5191
Stijn Janssens <janssens.stijn@hotmail.com>
2016-03-29 21:32:45 +0200

    sensors

--- code/js/websocket.js
+++ code/js/websocket.js
@@ -3,7 +3,7 @@ handlers = {}; // specify functions to deal with server messages (that aren't a 
 answers = {};  // specify functions that need to be called when the server answers
 
 function connect_to_websocket() {
-	websocket = new WebSocket("ws://" + window.house.host + "/ws");
+	websocket = new WebSocket("ws://" + window.location.host + "/ws");
 	//websocket = new WebSocket("ws://localhost:8002/ws");
 
 	websocket.request = function (requestObject, f) {
