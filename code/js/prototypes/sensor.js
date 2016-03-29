3cf4618be42679e5ab42597ca255ec597340aa5b
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-03-29 20:49:17 +0200

    Bugfix

--- code/js/prototypes/sensor.js
+++ code/js/prototypes/sensor.js
@@ -1,11 +1,11 @@
 Sensor.prototype = new DataType();
 Sensor.prototype.constructor = Sensor;
 
-function Sensor(SID, type, title, user_UID, location_LID, location_name) {
+function Sensor(SID, type, title, user_UID, location_LID) {
 	this.SID = SID;
 	this.type = type;
 	this.title = title;
 	this.user_UID = user_UID;
 	this.location_LID = location_LID;
-	this.location_name = location_name;
+	//this.location_name = location_name;
 }
