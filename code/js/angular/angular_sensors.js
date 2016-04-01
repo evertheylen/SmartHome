4a9a06d4368d754c3790f12f54c5166e324f0c63
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-01 14:04:35 +0200

    snackbar

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -239,6 +239,27 @@ angular.module("overwatch").controller("location_objController", function($scope
     };
 });
 
+		(function() {
+			'use strict';
+			var snackbarContainer = document.querySelector('#demo-snackbar-example');
+			var showSnackbarButton = document.querySelector('#demo-show-snackbar');
+			var handler = function(event) {
+				showSnackbarButton.style.backgroundColor = '';
+			};
+			showSnackbarButton.addEventListener('click', function() {
+				'use strict';
+				showSnackbarButton.style.backgroundColor = '#' +
+						Math.floor(Math.random() * 0xFFFFFF).toString(16);
+				var data = {
+					message: 'Button color changed.',
+					timeout: 2000,
+					actionHandler: handler,
+					actionText: 'Undo'
+				};
+				snackbarContainer.MaterialSnackbar.showSnackbar(data);
+			});
+		}());
+
 angular.module("overwatch").factory('dlgLocation_setup', function($rootScope) {
     var loc;
     return {
