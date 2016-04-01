d3d2583b99a97fe33cf07769e81dad0776dbdd37
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-01 14:24:01 +0200

    revert back to working state

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -136,6 +136,14 @@ angular.module("overwatch").controller("sensorController", function($scope, $roo
 	$scope.reset_sen();
 	$scope.reset_loc();
     
+	document.getElementById('btnLocationBack').addEventListener('click', function(){
+		document.getElementById('dlgLocation').close();
+	});
+
+	document.getElementById('btnSensorBack').addEventListener('click', function(){
+		document.getElementById('dlgSensor').close();
+	});
+
 	var delete_id = null;    // TODO Nasty global vars
 	var delete_from = null;
 	
@@ -174,29 +182,8 @@ angular.module("overwatch").controller("sensorController", function($scope, $roo
         element.showModal();
         $rootScope.$emit(elem + "_open");
         componentHandler.upgradeDom();
-    }
-		(function() {
-			'use strict';
-			var snackbarContainer = document.getElementById('demo-snackbar-example');
-			var showSnackbarButton = document.getElementById('demo-show-snackbar');
-			var handler = function(event) {
-				showSnackbarButton.style.backgroundColor = '';
-			};
-			showSnackbarButton.addEventListener('click', function() {
-				'use strict';
-				showSnackbarButton.style.backgroundColor = '#' +
-						Math.floor(Math.random() * 0xFFFFFF).toString(16);
-				var data = {
-					message: 'Button color changed.',
-					timeout: 2000,
-					actionHandler: handler,
-					actionText: 'Undo'
-				};
-				snackbarContainer.MaterialSnackbar.showSnackbar(data);
-			});
-			componentHandler.upgradeDom();
-		}());
-		
+    }  
+	
     componentHandler.upgradeDom();
 });
 
@@ -207,7 +194,7 @@ angular.module("overwatch").controller("location_objController", function($scope
         element.showModal();
         $rootScope.$emit("dlgLocation_open");
         componentHandler.upgradeDom();
-    };  
+    }  
 });
 
 angular.module("overwatch").factory('dlgLocation_setup', function($rootScope) {
@@ -358,9 +345,7 @@ angular.module("overwatch").controller("sensor_dialogController", function($scop
 		}
 		removeClass(document.getElementById(menu).parentNode, "is-visible");
 	}
-	document.getElementById('btnSensorBack').addEventListener('click', function(){
-		document.getElementById('dlgSensor').close();
-	});
+
 });
 	    
 angular.module("overwatch").controller("location_dialogController", function($scope, $rootScope, dlgLocation_setup) {
@@ -500,7 +485,4 @@ angular.module("overwatch").controller("location_dialogController", function($sc
 			console.log("Dialog closed");
 	    }
 	} 
-	document.getElementById('btnLocationBack').addEventListener('click', function(){
-		document.getElementById('dlgLocation').close();
-	});
 });
