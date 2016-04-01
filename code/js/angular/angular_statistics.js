d95c5923bb43c9ef71c2bc17417f174d3f895ae3
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-01 19:29:11 +0200

    Sample graphs in statistics tab

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -133,6 +133,24 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
     
     // Graphs
     
+    $scope.importants = [false, false, false, false, false, false];
+    var layout = document.getElementById("mainLayout");
+    if (hasClass(layout, "mdl-layout--no-drawer-button")) {
+        removeClass(layout, "mdl-layout--no-drawer-button");
+    }
+
+	$scope.mark_important = function mark_important(element_id) {
+	    var element = document.getElementById('important_icon-'+element_id);
+	    if (hasClass(element, "yellow")) {
+	        removeClass(element, "yellow");
+	        addClass(element, "white");
+	    } else if (hasClass(element, "white")) {
+	        removeClass(element, "white");
+	        addClass(element, "yellow");
+	    }
+	    $scope.importants[element_id] = !$scope.importants[element_id];
+	};
+    
     $scope.graphs = [];
     $scope.graphs_single = [];
     var graph_types = ["Line", "Bar", "Radar"];
