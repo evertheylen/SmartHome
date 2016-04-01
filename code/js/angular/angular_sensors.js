667d02ebe6da2cfddd5d2436713b5b5ea4ef48bf
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-01 14:19:32 +0200

    snackbar

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -174,14 +174,29 @@ angular.module("overwatch").controller("sensorController", function($scope, $roo
         element.showModal();
         $rootScope.$emit(elem + "_open");
         componentHandler.upgradeDom();
-    }  
-    		window.componentHandler.upgradeAllRegistered();
-    var snackbarContainer = document.getElementById("demo-snackbar-example");
-		var data = {
-				message: 'Button color changed.',
-				timeout: 2000,
-				actionText: 'Undo'
-		};    
+    }
+		(function() {
+			'use strict';
+			var snackbarContainer = document.getElementById('demo-snackbar-example');
+			var showSnackbarButton = document.getElementById('demo-show-snackbar');
+			var handler = function(event) {
+				showSnackbarButton.style.backgroundColor = '';
+			};
+			showSnackbarButton.addEventListener('click', function() {
+				'use strict';
+				showSnackbarButton.style.backgroundColor = '#' +
+						Math.floor(Math.random() * 0xFFFFFF).toString(16);
+				var data = {
+					message: 'Button color changed.',
+					timeout: 2000,
+					actionHandler: handler,
+					actionText: 'Undo'
+				};
+				snackbarContainer.MaterialSnackbar.showSnackbar(data);
+			});
+			componentHandler.upgradeDom();
+		}());
+		
     componentHandler.upgradeDom();
 });
 
