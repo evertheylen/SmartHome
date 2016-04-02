d89eed4e3c0bc20d378ab7b9c5c2b9eebeec93be
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-02 14:43:02 +0200

    Commencing on social tab

--- code/js/angular/angular_social.js
+++ code/js/angular/angular_social.js
@@ -4,3 +4,13 @@ angular.module("overwatch").controller("socialController", function($scope, $roo
     $rootScope.page_title = "OverWatch - " + $scope.i18n($rootScope.tab);
     componentHandler.upgradeDom();
 });
+
+angular.module("overwatch").controller("statusController", function($scope, $rootScope) {
+    $scope.comments = [];
+    
+    var comment = {};
+    comment.name = 'Adolf Hitler';
+    comment.text = 'Gutentag Poland, wir kommen für ihren arschen.';
+    comment.date = '01/09/1939';
+  
+});
