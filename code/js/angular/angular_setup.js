6b88dafbd144f21f4cca67795e355a64ced5237a
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-12 17:06:39 +0200

    Search friend

--- code/js/angular/angular_setup.js
+++ code/js/angular/angular_setup.js
@@ -153,6 +153,7 @@ angular.module("overwatch").controller("mainController", function($scope, $rootS
 	    if ($rootScope.tab != "") {
 	        $rootScope.page_title = "OverWatch - " + $scope.i18n($rootScope.tab);
 	    }
+      componentHandler.upgradeDom();
 	};
 	
 	$rootScope.types = ["electricity", "gas", "water"];
