796c15a906f4950710f71e02d876f0e726ff3771
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-12 16:52:20 +0200

    Search friend

--- code/js/angular/angular_setup.js
+++ code/js/angular/angular_setup.js
@@ -2,7 +2,7 @@
 //  Cookie for the internationalization
 
 
-angular.module("overwatch", ['ui.bootstrap', 'ngRoute', 'ngTagsInput', 'ngMessages', 'ngCookies', 'googlechart', 'chart.js', 'ui.router', 'angularjs-autocomplete'])
+angular.module("overwatch", ['ui.bootstrap', 'ngRoute', 'ngTagsInput', 'ngMessages', 'ngCookies', 'googlechart', 'chart.js', 'ui.router'])
     .directive('onFinishRender', function ($timeout) {
     return {
         restrict: 'A',
