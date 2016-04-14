dfaebfd214243d261d415411c06bdaafc04a8a3a
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-14 13:53:31 +0200

    Stats time input

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -310,9 +310,7 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
 	};
     
     $scope.graphs = [];
-    $scope.graphs_single = [];
     var graph_types = ["Line", "Bar", "Radar"];
-    var graph_types_single = ["Pie", "PolarArea", "Doughnut"];
     for (i=0; i < 3; i++) {
         graph = {};
         graph.type = graph_types[i];
@@ -324,27 +322,6 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
         ];
         //$scope.graphs.push(graph);  
     }
-    $timeout(function () {
-        for(i=0; i < 3; i++) {
-            $scope.graphs[i].data = [
-                [28, 48, 40, 19, 86, 27, 90, 59,54,53,30,12],
-                [65, 59, 80, 81, 56, 55, 40, 50,78,45,01,45]
-            ];
-        }        
-        
-    }, 5000);
-    for (i=0; i < 3; i++) {
-        graph = {};
-        graph.type = graph_types_single[i];
-        graph.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        graph.data = [65, 59,80,81,56,55,40,59,54,53,30,12];
-        $scope.graphs_single.push(graph);  
-    }   
-    $timeout(function () {
-        for (i=0; i < 3; i++) {
-            $scope.graphs_single[i].data = [28, 48, 40, 19, 86, 27, 90, 59,54,53,30,12];
-        }
-    }, 5000); 
     
     componentHandler.upgradeDom();
 });
