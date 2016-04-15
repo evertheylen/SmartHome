44ceadd6e50739918638239c8ee7eb7dc5ed1f8b
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-04-15 14:30:45 +0200

    Bugfix

--- code/js/angular/angular_social.js
+++ code/js/angular/angular_social.js
@@ -84,9 +84,19 @@ angular.module("overwatch").controller("find_friendsController", function($scope
 angular.module("overwatch").controller("create_groupController", function($scope, $rootScope) {
     $scope.create_group = function() {
         if ($scope.group_form.$valid) {
-            var group = new Group(-1, $scope.group_name, "desc", 0);
+            var wall = new Wall(-1);
+            delete wall.WID;
+            ws.request({
+                type: "add",
+                what: "Wall",
+                data: wall.toJSON()
+            }, function(response) {
+                wall = response.object;
+                $scope.$apply();
+            });
+
+            var group = new Group(-1, $scope.group_name, "desc", wall.WID);
             delete group.GID;
-            //delete group.wall_WID;
             // group.is_public = $scope.group_public;
             ws.request({
                 type: "add",
