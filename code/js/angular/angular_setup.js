c3d7d66bb8cbf3dc39cc43ac866847b43b015a83
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-19 20:53:46 +0200

    Admin

--- code/js/angular/angular_setup.js
+++ code/js/angular/angular_setup.js
@@ -51,6 +51,7 @@ angular.module("overwatch").run(function($rootScope, $location, Auth, $state) {
         } else if (Auth.isLoggedIn() &&$location.path() == "/admin" && !Auth.getUser().admin) {
             event.preventDefault();
             console.log("Gandalf calmly states that you have no rights to access these pages...\n'YOU SHALL NOT PASS - Gandalf'");
+            return $state.go(fromState.name);
             //$location.path('/');
             //$state.transitionTo(fromState.name);   */
         }else if ($location.path() != '/') {
