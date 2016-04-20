143e4ef51e9696e8b7a53a52225ff958d19ea854
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-04-20 14:35:02 +0200

    Bugfix

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -359,7 +359,7 @@ angular.module("overwatch").controller("sensor_dialogController", function($scop
 				// Edit Sensor 
                 // Add New Tags
                 for(var i = 0; i < $scope.sen_tags.length; i++) {
-                    var new_tag = new Tag($scope.sen_tag[i], $scope.sen_SID);
+                    var new_tag = new Tag($scope.sen_tags[i], $scope.sen_SID);
 			        ws.request({type: "add", what: "Tag", data: new_tag}, function(response) {
 				        response.object._scopes.push($scope);
 				        new_tag = response.object;
