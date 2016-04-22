1bf2878452c25d0a47d04db4152313db75030388
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-04-22 18:11:10 +0200

    Bugfix

--- code/js/websocket.js
+++ code/js/websocket.js
@@ -244,6 +244,8 @@ function getKey(type, data) {
             console.log(type + " recognized");
             var key = dataTypes[i].prototype._key;
             console.log("Key: " + key);
+            console.log("KeyLength: " + key.length);
+            console.log("Key[0]: " + key[0]);
             var tmp = [];
             for (j = 0; j < key.length; j++) 
                 tmp.push(data.key[j]);
