c90cc25ca951eb37526bff20b4bd7ec3657a3e23
stijnjanssens <janssens.stijn@hotmail.com>
2016-04-23 18:53:31 +0200

    tags

--- code/js/angular/angular_sensors.js
+++ code/js/angular/angular_sensors.js
@@ -307,8 +307,8 @@ angular.module("overwatch").controller("sensor_objController", function($scope, 
 	}
 
 	ws.request({type: "get_all", what: "Tag", for: {what: "Sensor", SID: $scope.sensor.SID}}, function(response) {
-		$scope.tags = response.objects;
-		temp_tags = JSON.stringify($scope.tags, null, 4); // (Optional) beautiful indented output.
+		$scope.sensor.tags = response.objects;
+		temp_tags = JSON.stringify($scope.sensor.tags, null, 4); // (Optional) beautiful indented output.
 		console.log("Tags for sensor " + $scope.sensor.title + ": " + temp_tags);
 		$scope.$apply();
 	});
