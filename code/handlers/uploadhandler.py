b3c670a7aa51cd28801f7bc4670103d609919c3d
Evert Heylen <evertheylen@gmail.com>
2016-04-23 18:05:28 +0200

    Upload handler

--- code/handlers/uploadhandler.py
+++ code/handlers/uploadhandler.py
@@ -1,16 +1,62 @@
+
+import io
+import csv
+from datetime import datetime
+
 import tornado
 
-# Handlers are instantiated for every request!
+from model import *
+
+wtf_dialect = csv.excel
+wtf_dialect.delimiter = ";"
+
+# Copy pasted from ElecSim
+# Because I don't want a single import of that horrendous code in this
+# project, here I prefer copy-pasting.
+
+class CsvSensor:
+    def __init__(self, name):
+        self.ID = int(name[name.find("[")+1:name.find("]")])
+        name = name[:name.find("[")] + name[name.find("]")+1:]
+        self.appname = name.strip()
+    
+    @property
+    def full_name(self):
+        return "{s.appname} [{s.ID}]".format(s=self)
+
+csv_date_format = "%Y-%m-%d %H:%M:%S"
+
 def create_UploadHandler(controller):
     class UploadHandler(tornado.web.RequestHandler):            
-        def post(self, *args):
-            print(self.request.files)
+        async def post(self, *args):
+            # TODO security
             fileinfo = self.request.files['file'][0]
             filename = fileinfo['filename']
             fbody = fileinfo['body']
-            insert_live = self.get_argument("live")
+            insert_live = len(self.get_arguments("live")) > 0
             
-            print(fbody)
+            f = io.StringIO(fbody.decode("utf-8"))
+            reader = csv.reader(f, dialect=wtf_dialect)
+            data = list(reader)
+            
+            sensors = []
+            for i, name in enumerate(data[0][2:-1]):
+                csv_sensor = CsvSensor(name)
+                sensor = await Sensor.find_by_key(csv_sensor.ID, controller.db)
+                sensors.append((i, CsvSensor(name)))
+                
+            
+            for (i, csv_sensor, sensor) in sensors:
+                for row in data[1:]:
+                    value = row[i]
+                    time = datetime.strptime(row[0], csv_date_format)
+                    v = Value(value=value, time=time, sensor=sensor)
+                    await v.insert(controller.db)
+            
+                    
+            
+            
+            
     
     return UploadHandler
 
