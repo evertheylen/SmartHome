8d80c5c8a73abea346c1b5d102d25db85340d95b
Evert Heylen <evertheylen@gmail.com>
2016-04-25 20:26:35 +0200

    friendships

--- code/model/value.py
+++ code/model/value.py
@@ -20,18 +20,18 @@ class Value(OwEntity):
 
 class HourValue(Value):
     @classmethod
-    async def aggregate(time, sensor):
-        # TODO aggregate :)
+    async def aggregate(time, sensor, db):
         # Step 1: get all values with Value.sensor == sensor and Value.time >= time and Value.time < time + 1 hour
+        result = await Value.get(Value.sensor == sensor, Value.time >= time, Value.time < time + 3600).exec(db)
+        values = result.all()
         # Step 2: determine average
-        # Step 3: Put those as HourValue, (always use starting point as 'time' attribute)
-        values = await Value.get(And(Value.sensor == sensor, Value.time >= time,Value.time < time + 1)).all(self.db) # not sure about +1 tough
-        count = await Value.get(And(Value.sensor == sensor, Value.time >= time,Value.time < time + 1)).count(self.db)
-        total = 0
-        total+= [v.value for v in values]
+        count = result.count()
+        total = sum([v.value for v in values])
         average = total/count
+        # Step 3: Put those as HourValue, (always use starting point as 'time' attribute)
         
-        pass
+        
+        
 
 class DayValue(Value):
     pass
