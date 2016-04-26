71eb756e32070cb2e26816de1a56510ddb49b97c
1 <1@1>
2016-04-26 09:43:46 +0200

    Bugfix

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -479,8 +479,8 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
         console.log("end time: " + $scope.end_date_time.value.getTime());
         console.log("start date: " + $scope.start_date.getTime());
         console.log("end date: " + $scope.end_date.getTime());
-        var timezone_offset = $scope.start_date.getTimezoneOffset()*60000;
-        var full_start_date = $scope.start_date.getTime() + $scope.start_date_time.value.getTime();
+        var timezone_offset = (1000*60*60) * 2;
+        var full_start_date = $scope.start_date.getTime() + $scope.start_date_time.value.getTime() + 2*timezone_offset;
         var full_end_date = $scope.end_date.getTime() + $scope.end_date_time.value.getTime() + 2*timezone_offset;
         var total_days = ($scope.end_date.getTime() - $scope.start_date.getTime()) / (1000*60*60*24);
         console.log("full start time: " + full_start_date);
