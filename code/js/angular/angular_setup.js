e4e236df6689ee5f7f2b8c27f8e9ef8a8b34bc0c
Stijn Janssens <janssens.stijn@hotmail.com>
2016-04-26 17:17:09 +0200

    Groups beginning

--- code/js/angular/angular_setup.js
+++ code/js/angular/angular_setup.js
@@ -278,6 +278,10 @@ angular.module("overwatch").config(["$stateProvider", "$urlRouterProvider", "$lo
 			url : "/social/find_friends",
 			templateUrl: "/html/partials/social_find_friends_tmp.html"
 		})
+		.state('social.state_group', {
+		    url : "/social/group",
+		    templateUrl: "/html/partials/social_group_tmp.html"
+		})
 		.state('state_admin', {
 			url : "/admin",
 			templateUrl: "/html/partials/admin_tmp.html"
@@ -289,10 +293,6 @@ angular.module("overwatch").config(["$stateProvider", "$urlRouterProvider", "$lo
 		.state('state_report', {
 			url : "/report",
 			templateUrl: "/html/partials/report_tmp.html"
-		})
-		.state('state_group', {
-		    url : "/group",
-		    templateUrl: "/html/partials/group_tmp.html"
 		});
 		
 	$locationProvider.html5Mode(true);
