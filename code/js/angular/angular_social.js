21309ae40bc00e34bc30e0fba93d7fe87d777da4
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-04-27 01:49:54 +0200

    Bugfix

--- code/js/angular/angular_social.js
+++ code/js/angular/angular_social.js
@@ -93,7 +93,8 @@ angular.module("overwatch").controller("statusIndexController", function ($scope
                             console.log("PUSHING STATUS");
                             $scope.statuses.push(response.objects[statusIndex]);
                         }
-                        console.log("DONE: " + $scope.statuses);
+                        for (int k = 0; k < $scope.statuses.length; k++)
+                            console.log("DONE" + k + ": " + $scope.statuses[k].text);
                     });
                     $scope.$apply();
                 });
