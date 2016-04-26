4c1b548f42cd2bc4c84ab749a8b5f838dcf57755
Stijn Janssens <janssens.stijn@hotmail.com>
2016-04-26 20:38:42 +0200

    stati

--- code/js/internationalization.js
+++ code/js/internationalization.js
@@ -119,4 +119,5 @@ var html_strings = {
     "pick_group" : ["Pick group", "Kies een groep"],
     "input_status_group": ["What do you want to share with the group?", "Wat ben je aan het doen?"],
     "post_group": ["Post in group", "Gooi het in de groep"],
+    "own_status" : ["Own status", "Eigen status"],
 };
