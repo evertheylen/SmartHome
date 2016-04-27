8685047b7594267bfc1b7e0e0d483f2352bbbbde
Stijn Janssens <janssens.stijn@hotmail.com>
2016-04-28 00:57:09 +0200

    Date bug

--- code/js/angular/angular_social.js
+++ code/js/angular/angular_social.js
@@ -912,7 +912,7 @@ angular.module("overwatch").controller("statusController", function($scope, $roo
             //comment.name = Auth.getUser().first_name + " " + Auth.getUser().last_name;
             //comment.text = $scope.new_comment;
             //comment.date = getCurrentDate();
-            var _date = Math.round(Date.now() / 1000);
+            var _date = Date.now();
             ws.request({
                 type: "add",
                 what: "Comment",
