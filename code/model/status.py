b93358b3f9cc04d631e87f0654f1e7408819537c
Evert Heylen <evertheylen@gmail.com>
2016-04-28 05:23:26 +0200

    komaan

--- code/model/status.py
+++ code/model/status.py
@@ -12,5 +12,5 @@ class Status(RTOwEntity):
     date = Property(int)
     date_edited = Property(int)  # If they are the same, no edits
     text = Property(str)
-    graph = Reference(Graph)
+    graph = Property(int, required=False)  # This is secretly a reference to Graph
     
