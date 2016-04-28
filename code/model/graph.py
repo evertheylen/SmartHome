2ce7bfe77b854dc2fd484c2de1a355026709b663
Evert Heylen <evertheylen@gmail.com>
2016-04-28 06:30:37 +0200

    komaan

--- code/model/graph.py
+++ code/model/graph.py
@@ -130,7 +130,7 @@ class Line(OwEntity):
     async def build(self, sensors, graph, db):
         self.sensors = sensors
         if len(self.sensors) == 0:
-            self.results = []
+            self.values = []
         else:
             req = RawSql("SELECT time, avg(value) AS value FROM {g.cls._table_name} WHERE sensor_SID IN {sensors} GROUP BY time HAVING time >= %(start)s AND time < %(end)s ORDER BY time".format(s=self, g=graph, sensors="("+str(self.sensors[0])+")" if len(self.sensors) == 1 else str(tuple(self.sensors))), {
                 "start": graph.timespan_start,
