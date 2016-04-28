7d2d5dfd9c427f1123cc574f1de22a2509e64229
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-04-28 05:42:49 +0200

    Bugfix

--- code/js/prototypes/status.js
+++ code/js/prototypes/status.js
@@ -2,12 +2,12 @@ Status.prototype = new DataType();
 Status.prototype.constructor = Status;
 Status.prototype._key = ["SID"];
 
-function Status(SID, date, date_edited, author_UID, wall_WID, text, graph_GID) {
+function Status(SID, date, date_edited, author_UID, wall_WID, text, graph) {
 	this.SID = SID;
 	this.date = date;
 	this.date_edited = date_edited;
 	this.author_UID = author_UID;
 	this.wall_WID = wall_WID;
 	this.text = text
-    this.graph_GID = graph_GID;
+    this.graph = graph;
 }
