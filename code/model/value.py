7184164a943febc9f445aca54d445aa55a8851cb
Evert Heylen <evertheylen@gmail.com>
2016-04-28 07:32:42 +0200

    komaan

--- code/model/value.py
+++ code/model/value.py
@@ -130,15 +130,15 @@ class Value(OwEntity):
             
             hourvalue_to_day_times = set()
             for t in hourvalue_times: 
-                hourvalue_to_day_times.add(HourValue.new_date(t))
+                hourvalue_to_day_times.add(HourValue.new_time(t))
                 
             dayvalue_to_month_times = set()
             for t in hourvalue_to_day_times: 
-                dayvalue_to_month_times.add(DayValue.new_date(t))
+                dayvalue_to_month_times.add(DayValue.new_time(t))
                 
             monthvalue_to_year_times = set()
             for t in dayvalue_to_month_times: 
-                monthvalue_to_year_times.add(MonthValue.new_date(t))
+                monthvalue_to_year_times.add(MonthValue.new_time(t))
             
             await HourValue.aggregate(sorted(hourvalue_to_day_times), sensor, db)
             await DayValue.aggregate(sorted(dayvalue_to_month_times), sensor, db)
