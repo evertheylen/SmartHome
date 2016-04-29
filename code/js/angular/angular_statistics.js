07c213fb1adbc2ccc5b012a9c77769d24f56a349
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-04-29 19:29:18 +0200

    Change to how graphs are converted

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -660,7 +660,16 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
     };
 
     $scope.set_graph_mode = function set_graph_mode(element_id) {
-        $scope.graphs[index].valueMode(true);
+        var element = document.getElementById('important_icon-' + element_id);
+        if (hasClass(element, "yellow")) {
+            $scope.graphs[index].valueMode(false);
+            removeClass(element, "yellow");
+            addClass(element, "white");
+        } else if (hasClass(element, "white")) {
+            $scope.graphs[index].valueMode(true);
+            removeClass(element, "white");
+            addClass(element, "yellow");
+        }
     };
 
     componentHandler.upgradeDom();
