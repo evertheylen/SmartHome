31b0b1696ee2277555a3be3ed13d2bae7c9a439b
babbel056 <hermans.anthony@hotmail.com>
2016-05-09 16:28:31 +0200

    change tags

--- code/model/tag.py
+++ code/model/tag.py
@@ -10,3 +10,10 @@ class Tag(RTOwEntity):
     async def is_authorized(self, type, usr, db, **kwargs):
         s = await Sensor.find_by_key(self.sensor).single(db)
         return s.user == usr.key
+
+    def json_repr(self):
+        return_value = {}
+        return_value["TID"] = self.TID
+        return_value["text"] = self.description
+        # self.__dict__[p.dataname]
+        return return_value
