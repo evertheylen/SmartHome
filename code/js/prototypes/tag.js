6160d9e19b9062e2871fef9549aba7515dab0f09
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-05-12 15:27:17 +0200

    Bugfix

--- code/js/prototypes/tag.js
+++ code/js/prototypes/tag.js
@@ -2,7 +2,7 @@ Tag.prototype = new DataType();
 Tag.prototype.constructor = Tag;
 Tag.prototype._key = ["sensor_SID"];
 
-function Tag(description, sensor_SID) {
-    this.description = description;
+function Tag(text, sensor_SID) {
+    this.text = text;
     this.sensor_SID = sensor_SID;
 }
