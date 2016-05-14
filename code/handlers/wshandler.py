a065204b0fe0bcd8271c23652be414d29e048894
Evert Heylen <evertheylen@gmail.com>
2016-05-14 15:54:34 +0200

    ref_obj wissel

--- code/handlers/wshandler.py
+++ code/handlers/wshandler.py
@@ -198,8 +198,8 @@ def create_WsHandler(controller, debug=True):
             """
             controller.ow.ioloop.spawn_callback(self.send, {
                 "type": "live_add_ref",
-                "from": ref_obj.json_key(),
-                "to": obj.json_key(),
+                "from": obj.json_key(),
+                "to": ref_obj.json_key(),
             })
             
         def remove_reference(self, obj, ref_obj):
@@ -208,8 +208,8 @@ def create_WsHandler(controller, debug=True):
             """
             controller.ow.ioloop.spawn_callback(self.send, {
                 "type": "live_remove_ref",
-                "from": ref_obj.json_key(),
-                "to": obj.json_key(),
+                "from": obj.json_key(),
+                "to": ref_obj.json_key(),
             })
         
 
