65d0dcc247c610c02b3158355e9cfc2f121b1c44
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-05-15 21:51:43 +0200

    Bugfix

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -651,7 +651,7 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
             case 'years':
                 time *= 60*24*30*365;
         }
-        var check_date = today - new Date(time);
+        var check_date = today - time;
         if (check_date.getYear() == today.getYear() && 
             check_date.getMonth() == today.getMonth() &&
             check_date.getDate() == today.getDate() ) {
