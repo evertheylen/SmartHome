454ff58823b5bf7146e7ba5683c9dd17bcc2fb75
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-05-15 16:24:26 +0200

    Bugfix

--- code/js/prototypes/graph.js
+++ code/js/prototypes/graph.js
@@ -20,12 +20,21 @@ function Graph(GID, timespan, group_by, where, lines, title) {
         var lines = this.lines;
         for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
             var values = lines[lineIndex].values;
-            if (values.length === 0) 
+            if (values.length === 0) { 
+                graph.data.push({data: []);
                 break;
-            graph.data.push({data: {x: values[0][0], y: values[0][1]}});
+            }
+            graph.data.push({data: [{x: values[0][0], y: values[0][1]}]});
             console.log("LineIndex: " + lineIndex);
             for (var valueIndex = 1; valueIndex < values.length; valueIndex++)
                 addPoint(graph, lineIndex, values[valueIndex][0], values[valueIndex][1]);
+
+function addPoint(graph, dataset, x, y) {
+    var last_point = graph.data[dataset].data[graph.data[dataset].data.length-1];
+    graph.data[dataset].data.push({x: x, y: last_point.y});
+    graph.data[dataset].data.push({x: x,y: y});
+}
+
             graph.data[lineIndex].label = lines[lineIndex].label;
         }
         this._graph = graph;
