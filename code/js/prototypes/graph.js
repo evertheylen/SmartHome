4a52c0f883c0be4dfb49aacb0b48c046243d622f
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-05-15 17:08:48 +0200

    Bugfix

--- code/js/prototypes/graph.js
+++ code/js/prototypes/graph.js
@@ -16,7 +16,7 @@ function Graph(GID, timespan, group_by, where, lines, title) {
     this.get_graph = function () {
         if (this._graph) 
             return this._graph;
-        var graph = {data: [], options: {bezierCurve: false, scaleType: "date", useUtc: false}};
+        var graph = {data: [], options: {bezierCurve: false, scaleType: "date", useUtc: false, scaleShowLabels: true}};
         var lines = this.lines;
         for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
             var values = lines[lineIndex].values;
