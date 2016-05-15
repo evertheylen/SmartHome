55d52cb6e66c94ece8ef8d673f28f9c94af1436c
stijnjanssens <janssens.stijn@hotmail.com>
2016-05-15 19:24:50 +0200

    live check

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -11,6 +11,11 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
     
     $scope.statistics = true;
 
+    $scope.set_live = function () {
+        console.log("clicked options");
+        console.log("$scope.live: " + $scope.live);
+    }
+    
     $scope.$on("ngRepeatFinishedGraphs", function(ngRepeatFinishedEvent) {
         for (i = 0; i< $scope.graphs.length; i++) {
             var ctx = document.getElementById("line-"+i).getContext("2d");
