6a533e3a2513f808124c1f843b828234a37206d3
stijnjanssens <janssens.stijn@hotmail.com>
2016-05-15 12:10:36 +0200

    Graph

--- code/js/angular/angular_main.js
+++ code/js/angular/angular_main.js
@@ -196,39 +196,8 @@ angular.module("overwatch").controller("mainController", function($scope, $rootS
 			document.getElementById("ScatterfullScreenDialog").showModal();
 			componentHandler.upgradeDom();
 			var ctx = document.getElementById("line-Scatterfullscreen").getContext("2d");
-      $scope.graph = new Chart(ctx).Scatter(graph.datasets, graph.options);
-var data = [
-    {
-      label: 'My First dataset',
-      strokeColor: '#F16220',
-      pointColor: '#F16220',
-      pointStrokeColor: '#fff',
-      data: [
-        { x: 19, y: 65 }, 
-        { x: 27, y: 59 }, 
-        { x: 28, y: 69 }, 
-        { x: 40, y: 81 },
-        { x: 48, y: 56 }
-      ]
-    },
-    {
-      label: 'My Second dataset',
-      strokeColor: '#007ACC',
-      pointColor: '#007ACC',
-      pointStrokeColor: '#fff',
-      data: [
-        { x: 19, y: 75, r: 4 }, 
-        { x: 27, y: 69, r: 7 }, 
-        { x: 28, y: 70, r: 5 }, 
-        { x: 40, y: 31, r: 3 },
-        { x: 48, y: 76, r: 6 },
-        { x: 52, y: 23, r: 3 }, 
-        { x: 24, y: 32, r: 4 }
-      ]
-    }
-  ];
 var options = {};
-$scope.scatter = new Chart(ctx).Scatter(data, options);
+$scope.scatter = new Chart(ctx).Scatter(graph.datasets, options);
     }
     
     $rootScope.Scatterclose_fullscreen = function() {
