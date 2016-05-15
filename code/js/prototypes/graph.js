7986eea73ca9c70ccb55767e0942830725a6712c
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-05-15 15:53:47 +0200

    Bugfix

--- code/js/prototypes/graph.js
+++ code/js/prototypes/graph.js
@@ -16,17 +16,16 @@ function Graph(GID, timespan, group_by, where, lines, title) {
     this.get_graph = function () {
         if (this._graph) 
             return this._graph;
-        this._graph = {};
-        this._graph.data = [];
-        this._graph.options = {bezierCurve: false};
+        graph = {data: [], options: {bezierCurve: false}};
         var lines = this.lines;
         for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
             var values = lines[lineIndex].values;
-            this._graph.data[lineIndex].data.push({x: values[0][0], y: values[0][1]});
+            graph.data.push({data: {x: values[0][0], y: values[0][1]}});
             for (var valueIndex = 1; valueIndex < values.length; valueIndex++)
-                addPoint(this._graph, lineIndex, values[valueIndex][0], values[valueIndex][1]);
-            this._graph.data[lineIndex].label = lines[lineIndex].label;
+                addPoint(graph, lineIndex, values[valueIndex][0], values[valueIndex][1]);
+            graph.data[lineIndex].label = lines[lineIndex].label;
         }
+        this._graph = graph;
         return this._graph;
     }
 }
