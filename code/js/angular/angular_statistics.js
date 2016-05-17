58ba564425b87bb86a7522a25b6b73d81e8638c7
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-05-17 18:17:50 +0200

    Bugfix

--- code/js/angular/angular_statistics.js
+++ code/js/angular/angular_statistics.js
@@ -791,7 +791,7 @@ angular.module("overwatch").controller("statisticsController", function($scope, 
                     console.log("Lines: " + lines);
                     for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
                         console.log("LineIndex: " + lineIndex);
-                        var values = lines[lineIndex].values;
+                        var values = lines[lineIndex]["values"];
                         for (var valueIndex = 0; valueIndex < values.length; valueIndex++) {
                             addPoint(graph, graph.line_map[lines[lineIndex].LLID], values[valueIndex][1], values[valueIndex][0]);
                         }
