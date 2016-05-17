6bce98b1ec8bb860d57d0e87a13a17bfb2054494
jeroenverstraelen <jeroenverstraelen@hotmail.com>
2016-05-17 18:02:05 +0200

    Bugfix

--- code/js/prototypes/liveGraph.js
+++ code/js/prototypes/liveGraph.js
@@ -19,8 +19,11 @@ function LiveGraph(LGID, timespan, group_by, where, lines, title) {
             return this._graph;
         var graph = {temp_GID: this.LGID, live: true, data_type: cache["LiveGraph"][this.LGID], data: [], options: {bezierCurve: false, scaleType: "date", useUtc: false, scaleShowLabels: true, line_map: {}}};
         var lines = this.lines;
+        console.log("Iterating lineIndex");
         for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
+            console.log("Setting line_map");
             graph.line_map[lines[lineIndex].LLID] = lineIndex;
+            console.log("Setting graph data");
             graph.data[lineIndex] = {label: "", strokeColor: ""};
             graph.data[lineIndex].label = lines[lineIndex].label;
             graph.data[lineIndex].strokeColor = 'rgb(' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ', ' + Math.floor(Math.random() * 255) + ')';
