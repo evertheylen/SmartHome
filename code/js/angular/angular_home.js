bf3ca218e0e130ffbe89bb9a3dec15e886baf60e
stijnjanssens <janssens.stijn@hotmail.com>
2016-05-17 20:27:22 +0200

    Scatter graphs

--- code/js/angular/angular_home.js
+++ code/js/angular/angular_home.js
@@ -89,6 +89,16 @@ angular.module("overwatch").controller("homeController", function($scope, $rootS
                 }, function(valueResponse) {
                     var graph = cache["LiveGraph"][valueResponse["LGID"]].get_graph();
                     var lines = valueResponse.lines;
+                    graph.options = {
+                        bezierCurve: false,
+                        responsive: false,
+                        maintainAspectRatio: false,
+                        scaleType: "date",
+                        scaleDateFormat: "dd mmm yy",
+                        scaleTimeFormat: "h:MM",
+                        useUtc: true,
+                        animation: false
+                    }
                     for (var lineIndex = 0; lineIndex < lines.length; lineIndex++) {
                         var values = lines[lineIndex].values;
                         for (var valueIndex = 0; valueIndex < values.length; valueIndex++)
@@ -149,43 +159,6 @@ angular.module("overwatch").controller("homeController", function($scope, $rootS
         }
     });
   
-    $scope.graphs = [];
-    $scope.graphs_single = [];
-    var graph_types = ["Line", "Bar", "Radar"];
-    var graph_types_single = ["Pie", "PolarArea", "Doughnut"];
-    for (i=0; i < 3; i++) {
-        graph = {};
-        graph.type = graph_types[i];
-        graph.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        graph.series = ['Series A', 'Series B'];
-        graph.data = [
-            [65, 59,80,81,56,55,40,59,54,53,30,12],
-            [28,48,40,19,86,27,90,40,78,45,01,45]
-        ];
-        $scope.graphs.push(graph);  
-    }
-    $timeout(function () {
-        for(i=1; i < 3; i++) {
-            $scope.graphs[i].data = [
-                [28, 48, 40, 19, 86, 27, 90, 59,54,53,30,12],
-                [65, 59, 80, 81, 56, 55, 40, 50,78,45,01,45]
-            ];
-        }        
-        
-    }, 5000);
-    for (i=0; i < 3; i++) {
-        graph = {};
-        graph.type = graph_types_single[i];
-        graph.labels = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        graph.data = [65, 59,80,81,56,55,40,59,54,53,30,12];
-        $scope.graphs_single.push(graph);  
-    }   
-    $timeout(function () {
-        for (i=0; i < 3; i++) {
-            $scope.graphs_single[i].data = [28, 48, 40, 19, 86, 27, 90, 59,54,53,30,12];
-        }
-    }, 5000); 
-
 	componentHandler.upgradeDom();
 });
 
