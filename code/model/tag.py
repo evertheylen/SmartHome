bd849656db9e846c0e8780473505a09c60ed6be2
Evert Heylen <evertheylen@gmail.com>
2016-05-17 22:44:19 +0200

    tags

--- code/model/tag.py
+++ code/model/tag.py
@@ -7,9 +7,6 @@ class Tag(RTOwEntity):
     description = Property(str)
     key = TID = KeyProperty()
 
-    async def is_authorized(self, usr, sensor, db):
-        s = await Sensor.find_by_key(sensor, db)
-        return s.user == usr
 
     def json_repr(self):
         return {
